059c6e644f9f05bed75479030023c5eb6af484d3
Adam Ben Kacem <adambkd@hotmail.com>
2026-09-03 21:06:40 +0200

    Setup basic class skeleton

---
+++ BlockingQueue.hpp
@@ -0,0 +1,31 @@
+#include <cstddef>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+template<typename T>
+class BlockingQueue {
+    private:
+        std::queue<T> queue_;
+        size_t capacity_;
+
+    public:
+        explicit BlockingQueue(size_t capacity) : capacity_(capacity) {}
+
+        void enqueue(const T& value) {
+            if (isFull()) {
+                return;
+            }
+
+            queue_.push(value);
+        }
+
+        T dequeue();
+
+        size_t size() const {
+            return queue_.size();
+        }
+
+        bool isEmpty() const;
+        bool isFull() const;
+};
