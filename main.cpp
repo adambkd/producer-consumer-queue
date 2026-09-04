c2839e3270a15e6480933c4e45e93db4ca29ea85
Adam Ben Kacem <adambkd@hotmail.com>
2026-09-04 13:12:47 +0200

    Testing in main function with producer thread and consumer thread and iterating > capacity

--- main.cpp
+++ main.cpp
@@ -1,16 +1,34 @@
 #include "BlockingQueue.hpp"
+#include <mutex>
+#include <thread>
 #include <iostream>
 
 int main() {
 
-BlockingQueue<int> obj1(3);
+BlockingQueue<int> queue(5);
 
-obj1.enqueue(5);
-obj1.enqueue(9);
-obj1.enqueue(1);
+std::mutex coutMutex;
 
-std::cout << obj1.size();
+std::thread producer([&queue, &coutMutex](){
+    for (int i = 1; i <= 10; i++) {
+        queue.enqueue(i);
 
+        std::lock_guard<std::mutex> lock(coutMutex);
+        std::cout << "Producer produced: " << i << "\n";
+    }
+});
+
+std::thread consumer([&queue, &coutMutex](){
+    for (int i = 1; i <= 10; i++) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        int value = queue.dequeue();
+        std::lock_guard<std::mutex> lock(coutMutex);
+        std::cout << "Consumer consumed: " << value << "\n";
+    }
+});
+
+producer.join();
+consumer.join();
 
     return 0;
 }
